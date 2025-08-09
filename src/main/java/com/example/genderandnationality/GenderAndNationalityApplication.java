3d3b077582ac6cc9fed1640937cf1115a46bfd28
Anita889 <manahit889@gmail.com>
2025-08-09 16:23:45 +0400

    First commit

---
+++ src/main/java/com/example/genderandnationality/GenderAndNationalityApplication.java
@@ -0,0 +1,13 @@
+package com.example.genderandnationality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderAndNationalityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenderAndNationalityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/genderandnationality/GenderAndNationalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.genderandnationality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderAndNationalityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
